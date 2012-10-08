bea4d4d08882f850a0f881fee5d69fec17ef1a70
Pablo del Sel <pablodelsel@gmail.com>
2012-10-08 17:22:09 -0300

    * iOS host

---
+++ ios/lua/src/characters.lua
@@ -0,0 +1,12 @@
+-- --==============================================================
+-- -- The Insulines
+-- -- Copyright (c) 2010-2012 quov.is
+-- -- All Rights Reserved. 
+-- -- http://quov.is // http://theinsulines.com
+-- --==============================================================
+-- 
+
+-- mainCharacter = character.new ( 'main_character' )
+-- mainCharacter.renderPriority = 200
+-- nancy = character.new ( 'nancy' )
+-- nancy.renderPriority = 100
